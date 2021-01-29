f383d1a803a590f66149a14febf782b5e054a43a
Jacob Carr <jcarr343@hotmail.co.uk>
2021-01-29 22:20:51 +0000

    Basic camera follow/grid

---
+++ New Unity Project/Assets/CameraGrid.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGrid : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)  //very basic, needs to be improved
+    {
+        int multX = 1;
+        if (collision.transform.position.x < transform.position.x)
+        {
+            multX = -1;
+        }
+        if (collision.CompareTag("Player"))
+        {
+            float x = (collision.gameObject.transform.position.x + 13.25f * multX);
+            transform.position = new Vector3(x, 0, -10);
+        }
+    }
+}
