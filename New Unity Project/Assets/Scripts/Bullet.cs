6c9fc3b152371f71aeac793a87dc3eeee0f14ca6
Jonathan <10jfarr@gmail.com>
2021-01-30 21:38:11 +0000

    bullet fixing

--- New Unity Project/Assets/Scripts/Bullet.cs
+++ New Unity Project/Assets/Scripts/Bullet.cs
@@ -20,33 +20,40 @@ public class Bullet : MonoBehaviour
 
     void Start()
     {
+        bullet = FMODUnity.RuntimeManager.CreateInstance("event:/Enemy2/Bullet");
+        bullet.setParameterByName("BulletHit", 0);
+        bullet.start();
+
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindObjectOfType<Player>();
         moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
         rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
         //rb.velocity = moveDirection.normalized;
         Destroy(gameObject, 7f);
-
-        bullet = FMODUnity.RuntimeManager.CreateInstance("event:/Enemy2/Bullet");
-        bullet.start();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        bullet.setParameterByName("BulletHit", 1);
+
         if (collision.gameObject.tag == "Player")
         {
             //playersHealth.GetComponent<Player>().DealDamage(10f);
             collision.gameObject.GetComponent<Player>().DealDamage(damage / 2);
             Debug.Log("HIT");
-            Destroy(gameObject);
 
             bullet.setParameterByName("BulletCollision", 1);
             bullet.start();
+
+            Destroy(gameObject);
         }
         else
         {
+            bullet.setParameterByName("BulletHit", 1);
             bullet.setParameterByName("BulletCollision", 0);
             bullet.start();
+
+            Destroy(gameObject);
         }
     }
 
