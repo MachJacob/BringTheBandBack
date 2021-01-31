74202f7334d735020bc2fb9fe15b99a34988578f
Jonathan <10jfarr@gmail.com>
2021-01-31 13:37:59 +0000

    menue sounds

--- New Unity Project/Assets/Scripts/SceneSwitcher.cs
+++ New Unity Project/Assets/Scripts/SceneSwitcher.cs
@@ -6,14 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-
+    FMOD.Studio.EventInstance Loadsound;
+    FMOD.Studio.EventInstance Quitsound;
+    
     public void LoadScene(string sceneName)
     {
+        Loadsound = FMODUnity.RuntimeManager.CreateInstance("event:/UI/Play");
+        Loadsound.start();
         SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame()
     {
+        Quitsound = FMODUnity.RuntimeManager.CreateInstance("event:/Player/Jump");
+        Quitsound.start();
         Application.Quit();
     }
 }
