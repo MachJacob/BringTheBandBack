7a53eb0d8080e21af2fae4e500350ccc960ba89c
Jonathan <10jfarr@gmail.com>
2021-01-31 13:58:25 +0000

    UI sounds Extensiton

--- New Unity Project/Assets/Scripts/GameOver.cs
+++ New Unity Project/Assets/Scripts/GameOver.cs
@@ -5,14 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    FMOD.Studio.EventInstance click;
+    void Start()
+    {
+        click = FMODUnity.RuntimeManager.CreateInstance("event:/UI/Click");
+    }
     public void OnReplayPressed()
     {
         // switcher.LoadScene()
         SceneManager.LoadScene(1);
+        click.start();
     }
 
     public void OnMenuPressed()
     {
         SceneManager.LoadScene(0);
+        click.start();
     }
 }
